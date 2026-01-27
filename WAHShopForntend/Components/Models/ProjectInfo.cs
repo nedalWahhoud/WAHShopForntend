37c726f2d955499456a3f3d90cdb0fe0a76508bc
nedalWahhoud <nedal.1992.29@gmail.com>
2026-01-27 15:36:59 +0100

    update

--- WAHShopForntend/Components/Models/ProjectInfo.cs
+++ WAHShopForntend/Components/Models/ProjectInfo.cs
@@ -2,14 +2,11 @@
 {
     public class ProjectInfo
     {
-        public string? Name { get; set; }
-        public string? NameArabic { get; set; }
-        public string? Address { get; set; }
-        public string? Latitude { get; set; }
-        public string? Longitude { get; set; }
-        public string? Whatsapp { get; set; }
-        public string? Email { get; set; }
-        public string? Facebook { get; set; }
-        public string? Instagram { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Latitude { get; set; } = string.Empty;
+        public string Longitude { get; set; } = string.Empty;
+        public string Whatsapp { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
