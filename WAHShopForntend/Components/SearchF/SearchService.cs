89f61f1a070e4eabd9e28690af40ae043f08391f
nedalWahhoud <nedal.1992.29@gmail.com>
2026-04-21 19:46:56 +0200

    SuppliersPage

--- WAHShopForntend/Components/SearchF/SearchService.cs
+++ WAHShopForntend/Components/SearchF/SearchService.cs
@@ -27,7 +27,7 @@ namespace WAHShopForntend.Components.SearchF
                 String query = $"?query={Uri.EscapeDataString(searchText.Trim())}";
                 if (DownloadedProductExistingIds != null && DownloadedProductExistingIds.Count != 0)
                 {
-                    // تحويل القائمة إلى سلسلة مثل excludeCategoryIds=1&excludeCategoryIds=2
+                    // Die Liste in einen String umwandeln, z. B. excludeCategoryIds=1&excludeCategoryIds=2
                     var excludedQueryPart = string.Join("&", DownloadedProductExistingIds.Select(id => $"excludeIds={id}"));
                     query += "&" + excludedQueryPart;
                 }
@@ -58,17 +58,12 @@ namespace WAHShopForntend.Components.SearchF
             try
             {
                 return _productService.DownloadedProduct
-               .Where(p =>
-              !excludeIds.Contains(p.Id) && (
-              (p.Name_de != null && p.Name_de.Contains(searchText.Trim(), StringComparison.OrdinalIgnoreCase)) ||
-              (p.Description_de != null && p.Description_de.Contains(searchText, StringComparison.OrdinalIgnoreCase)) ||
-               (p.Name_ar != null && p.Name_ar.Contains(searchText.Trim(), StringComparison.OrdinalIgnoreCase)) ||
-              (p.Description_ar != null && p.Description_ar.Contains(searchText, StringComparison.OrdinalIgnoreCase)) ||
-              (p.Category != null && p.Category.Name_de != null && p.Category.Name_de.Contains(searchText, StringComparison.OrdinalIgnoreCase)) ||
-              (p.Category != null && p.Category.Name_ar != null && p.Category.Name_ar.Contains(searchText, StringComparison.OrdinalIgnoreCase))
-               )
-             )
-            .ToList();
+               .Where(p => !excludeIds.Contains(p.Id) && (
+                     (p.Name_de != null && p.Name_de.Contains(searchText.Trim(), StringComparison.OrdinalIgnoreCase)) ||
+                     (p.Description_de != null && p.Description_de.Contains(searchText, StringComparison.OrdinalIgnoreCase)) ||
+                     (p.Name_ar != null && p.Name_ar.Contains(searchText.Trim(), StringComparison.OrdinalIgnoreCase)) ||
+                     (p.Description_ar != null && p.Description_ar.Contains(searchText, StringComparison.OrdinalIgnoreCase))))
+               .ToList();
 
             }
             catch
